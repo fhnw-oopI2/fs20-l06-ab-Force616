82c089f77d8796cdd441d19a25039e4039c3dcd3
Stefan Buehler (s) <stefan.buehler@students.fhnw.ch>
2020-03-31 10:56:29 +0200

    AB2

---
+++ src/main/java/ch/fhnw/module06/ab1/ApplicationUI.java
@@ -0,0 +1,39 @@
+package ch.fhnw.module06.ab1;
+
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ApplicationUI extends StackPane{
+	
+	private Button button;
+	private TextField textField1;
+	private TextField textField2;
+
+	
+	public ApplicationUI() {
+		initalizeControls();
+		layoutControls();
+	}
+	
+	private void initalizeControls() {
+		button = new Button();
+		textField1 = new TextField("Title");
+		textField2 = new TextField("Description");
+
+		
+	}
+	
+	private void layoutControls() {
+		VBox rootPane = new VBox();
+		rootPane.setPadding(new Insets(10,10,10,10));
+		rootPane.getChildren().add(textField1);
+		rootPane.getChildren().add(textField2);
+		rootPane.getChildren().add(button);
+	}
+	
+}
