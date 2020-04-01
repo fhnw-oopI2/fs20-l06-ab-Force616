ad5efff0d3e4b2f54915fd405cc960d1733d3470
Stefan Buehler (s) <stefan.buehler@students.fhnw.ch>
2020-04-01 19:59:25 +0200

    Stackpane

--- src/main/java/ch/fhnw/module06/ab2/ApplicationUI.java
+++ src/main/java/ch/fhnw/module06/ab2/ApplicationUI.java
@@ -8,7 +8,6 @@ import javafx.scene.layout.VBox;
 
 public class ApplicationUI extends StackPane{
 	
-	
 	private Button button;
 	private TextField textField1;
 	private TextField textField2;
@@ -27,8 +26,10 @@ public class ApplicationUI extends StackPane{
 	
 	private void layoutControls() {
 		VBox rootPane = new VBox();
+		StackPane stackPane = new StackPane();
 		rootPane.setPadding(new Insets(10));
+		stackPane.getChildren().add(rootPane);
+
 		rootPane.getChildren().addAll(textField1,textField2,button);
-		
 	}
 }
