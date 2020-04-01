7ee27bb949750f6c549e1e6979dcc7210ec44aa5
Stefan Buehler (s) <stefan.buehler@students.fhnw.ch>
2020-04-01 12:32:05 +0200

    Nun habe ich alles zum laufen gebracht, jetzt wird das Fenster nicht befüllt. Ich finde den Fehler nicht.

--- src/main/java/ch/fhnw/module06/ab2/ApplicationUI.java
+++ src/main/java/ch/fhnw/module06/ab2/ApplicationUI.java
@@ -1,14 +1,13 @@
 package ch.fhnw.module06.ab2;
 
 import javafx.geometry.Insets;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 public class ApplicationUI extends StackPane{
+	
 	
 	private Button button;
 	private TextField textField1;
@@ -24,16 +23,12 @@ public class ApplicationUI extends StackPane{
 		button = new Button();
 		textField1 = new TextField("Title");
 		textField2 = new TextField("Description");
-
-		
 	}
 	
 	private void layoutControls() {
 		VBox rootPane = new VBox();
-		rootPane.setPadding(new Insets(10,10,10,10));
-		rootPane.getChildren().add(textField1);
-		rootPane.getChildren().add(textField2);
-		rootPane.getChildren().add(button);
+		rootPane.setPadding(new Insets(10));
+		rootPane.getChildren().addAll(textField1,textField2,button);
+		
 	}
-	
 }
