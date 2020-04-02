7b3108e5004ae490888f27db6a548d8e5a131e53
Stefan Buehler (s) <stefan.buehler@students.fhnw.ch>
2020-04-02 18:42:46 +0200

    Working

--- src/main/java/ch/fhnw/module06/ab2/ApplicationUI.java
+++ src/main/java/ch/fhnw/module06/ab2/ApplicationUI.java
@@ -26,10 +26,8 @@ public class ApplicationUI extends StackPane{
 	
 	private void layoutControls() {
 		VBox rootPane = new VBox();
-		StackPane stackPane = new StackPane();
 		rootPane.setPadding(new Insets(10));
-		stackPane.getChildren().add(rootPane);
-
 		rootPane.getChildren().addAll(textField1,textField2,button);
+		this.getChildren().add(rootPane);
 	}
 }
